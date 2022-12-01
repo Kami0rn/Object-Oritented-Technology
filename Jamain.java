caa33d7b0d8e0ae507aec770ecb969a8282c8111
Kami0rn <74646193+Kami0rn@users.noreply.github.com>
2022-12-01 11:07:40 +0700

    Add files via upload

--- Jamain.java
+++ Jamain.java
@@ -4,10 +4,13 @@ import java.util.Scanner;
 
 public class Jamain { //pascal caase
     public static void main(String[] args){
+        /* 
         Scanner sc = new Scanner(System.in);
         String name ;
         System.out.print("input name : ");
         name = sc.nextLine();
-        System.out.print(name);
+        System.out.print("Name bull is "+name);*/
+        System.out.print(args[0]);
+
     }
 }
